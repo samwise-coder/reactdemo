710cc3e20b4a7f0cfc3d201a3c307a54854d2775
samwise-coder <974049754@qq.com>
2021-09-17 23:33:58 +0800

    add state

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 // import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>hello~ React</h1>
-      <h1>hello~ React</h1>
-      <h1>hello~ React</h1>
-
-    </div>
-  );
+class Clock extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { date: new Date() }
+  }
+  render() {
+    return (
+      <div>
+        <h1>hello react</h1>
+        <h2>it is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Clock;
